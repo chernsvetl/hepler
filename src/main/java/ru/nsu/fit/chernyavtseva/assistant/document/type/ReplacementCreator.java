efc601ba35eee8f05cb8d64ffe84f99b688ad346
Svetlana Chernyavtseva <chernsvetl>
2023-04-26 00:42:51 +0700

    uri changes

--- src/main/java/ru/nsu/fit/chernyavtseva/assistant/document/type/ReplacementCreator.java
+++ src/main/java/ru/nsu/fit/chernyavtseva/assistant/document/type/ReplacementCreator.java
@@ -5,8 +5,10 @@ import com.github.petrovich4j.Gender;
 import com.github.petrovich4j.NameType;
 import com.github.petrovich4j.Petrovich;
 import com.hp.hpl.jena.query.QuerySolution;
-import ru.nsu.fit.chernyavtseva.assistant.document.type.course2.mda.MasterIndividualTaskMDA;
-import ru.nsu.fit.chernyavtseva.assistant.document.type.course2.trps.MasterIndividualTaskTRPS;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Extracts data from solution and normalizes it if needed
@@ -14,14 +16,35 @@ import ru.nsu.fit.chernyavtseva.assistant.document.type.course2.trps.MasterIndiv
 public sealed interface ReplacementCreator {
 
     static ReplacementCreator simple(String solutionVarName) {
-        return new Simple(solutionVarName);
+        return new UriConverterReplacement(new Simple(solutionVarName));
     }
 
     static ReplacementCreator fullName(String solutionVarName, Case wordCase) {
-        return new FullNameReplacement(solutionVarName, wordCase);
+        return new UriConverterReplacement(new FullNameReplacement(solutionVarName, wordCase));
     }
 
     String replacement(QuerySolution solution);
+}
+
+final class UriConverterReplacement implements ReplacementCreator {
+
+    private static final char[] ENCODED_CHARS = {'(', ')', '"', '«', '»'};
+
+    private final ReplacementCreator delegateTo;
+
+    UriConverterReplacement(ReplacementCreator delegateTo) {
+        this.delegateTo = delegateTo;
+    }
+
+    @Override
+    public String replacement(QuerySolution solution) {
+        String replacement = delegateTo.replacement(solution);
+        for (char c : ENCODED_CHARS) {
+            String encoded = URLEncoder.encode(String.valueOf(c), StandardCharsets.UTF_8);
+            replacement = replacement.replaceAll(encoded, String.valueOf(c));
+        }
+        return replacement;
+    }
 }
 
 final class Simple implements ReplacementCreator {
@@ -53,29 +76,9 @@ final class FullNameReplacement implements ReplacementCreator {
     public String replacement(QuerySolution solution) {
         String fullName = solution.getLiteral(solutionVarName).getString();
         String[] nameChunks = fullName.split(" ");
-
-
-
-
-        /* can't genarate docs for define profile */
-        if(solution.get("профиль").asLiteral().getString().equals("Технология разработки программных систем")){
-            // to generate docs for people, who has profile = Технология разработки программных систем
-            new MasterIndividualTaskTRPS();
-            System.out.println(solution.get("фио_студента") + " " +  solution.get("профиль"));
-
-        } else if(solution.get("профиль").asLiteral().getString().equals("Компьютерное моделирование и анализ данных")){
-            // to generate docs for people, who has profile = Компьютерное моделирование и анализ данных
-            new MasterIndividualTaskMDA();
-            System.out.println(solution.get("фио_студента") + " " +  solution.get("профиль"));
-        }
-
-
-
-
         if (nameChunks.length == 4) {
             return nameChunks[0] + 'а' + " " + nameChunks[1] + 'а' + " " + nameChunks[2] + " " + nameChunks[3];
-        }
-        else if (nameChunks.length == 3) {
+        } else if (nameChunks.length == 3) {
             Petrovich petrovich = new Petrovich();
             Gender gender = petrovich.gender(nameChunks[2], Gender.Both);
             String lastName = petrovich.say(nameChunks[0], NameType.LastName, gender, wordCase);
