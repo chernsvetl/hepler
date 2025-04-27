1ef0d9161ee5fc521b2ea9a7527a7853a65634e5
Svetlana Chernyavtseva <chernsvetl@mail.ru>
2025-04-27 23:44:53 +0700

    исправила пути до файлов

--- src/main/java/normative_control/utils/Files.java
+++ src/main/java/normative_control/utils/Files.java
@@ -6,7 +6,7 @@ public class Files {
     public static String KNIS_8_INDIVIDUAL_TASK_FILE = "validation_log/individual_task_bachelors_knis_fourth_year_students_8th_semester";
     public static String IIIDS_4_INDIVIDUAL_TASK_FILE = "validation_log/individual_task_masters_iiids_second_year_students_4th_semester";
     public static String IOT_4_INDIVIDUAL_TASK_FILE = "validation_log/individual_task_masters_iot_second_year_students_4th_semester";
-    public static String TRPS_4_INDIVIDUAL_TASK_FILE = "validation_log/individual_task_masters_trps_fourth_year_students_4th_semester";
+    public static String TRPS_4_INDIVIDUAL_TASK_FILE = "validation_log/individual_task_masters_trps_second_year_students_4th_semester";
 
     // REPORT
     public static String PIIKN_8_REPORT_FILE = "validation_log/report_bachelors_piikn_fourth_year_students_8th_semester";
